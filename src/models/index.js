0535086b2b459075c1a4d4183232a8f531b52d87
Pedro Melém <melem.dev@gmail.com>
2022-03-16 03:11:58 -0300

    fix: export models

---
+++ src/models/index.js
@@ -0,0 +1,16 @@
+const MClient = require("./client.model");
+const MEmployee = require("./employee.model");
+const MOrder = require("./order.model");
+const MProduct = require("./product.model");
+const MUser = require("./user.model");
+
+const MOrderItem = require("./order.item.model");
+
+module.exports = {
+  MClient,
+  MEmployee,
+  MOrder,
+  MProduct,
+  MUser,
+  MOrderItem,
+};
